d7a04dfa27e94b82f527fd799ad34f165d1b5dbc
Ahmed-Elnemr <ahmedelnemr064@gmail.com>
2023-12-19 10:37:34 +0200

    dashboard admin install

---
+++ app/Http/Controllers/Dshboard/AdminDashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Dshboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminDashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        if(view()->exists($id)){
+            return view( $id);
+        }
+        else
+        {
+            return view('dashboard.404');
+        }
+
+     //   return view($id);
+    }
+}
